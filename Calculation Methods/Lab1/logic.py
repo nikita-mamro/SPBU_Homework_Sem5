534e505e3aee8128af099e499fb6f8285abb0830
Nikita Mamro <nikita.mamro@lanit-tercom.com>
2020-09-05 12:40:27 +0300

    Added given constants and function declaration and printing

--- Calculation Methods/Lab1/logic.py
+++ Calculation Methods/Lab1/logic.py
@@ -1,2 +1,12 @@
+import settings
+
+
 def print_title():
-    print("Lab 1")
+    print("Lab 1\n")
+    print("f(x) = ", settings.F_STRING)
+    print("[A, B] = [", settings.A, ",", settings.B, "]")
+    print("\u03B5 = ", settings.EPSILON)
+
+
+def root_separation():
+    return 0
---
+++ Calculation Methods/Lab1/settings.py
@@ -0,0 +1,10 @@
+import math
+
+EPSILON = 10 ** -5
+A = -8
+B = 2
+F_STRING = "10 * cos(x) - 0.1 * x\u00b2"
+
+
+def f(x):
+    10 * math.cos(x) - 0.1 * x ** 2
