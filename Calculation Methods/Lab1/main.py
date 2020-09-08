edf3b196d163ed161f2fffa966a4c59c1a97a7f9
Nikita Mamro <mamronikita@gmail.com>
2020-09-08 11:43:03 +0300

    Update main.py

--- Calculation Methods/Lab1/main.py
+++ Calculation Methods/Lab1/main.py
@@ -1,6 +1,7 @@
 import printer
 import sys
 import colors
+import settings
 
 
 def main():
@@ -10,8 +11,8 @@ def main():
 
     for section in sections:
         sys.stdout.write(colors.BLUE)
-        print("\nОтрезок [", round(section[0], 6),
-              ",", round(section[1], 6), "]")
+        print("\nОтрезок [", round(section[0], settings.ROUNDING_DIGITS),
+              ",", round(section[1], settings.ROUNDING_DIGITS), "]")
         sys.stdout.write(colors.RESET)
         printer.print_section_methods(section)
 
