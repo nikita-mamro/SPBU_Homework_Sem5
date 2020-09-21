7f290680ccaac83e9665b82e634fe8724edd753e
Nikita Mamro <mamronikita@gmail.com>
2020-09-21 22:25:28 +0300

    Added option to iterate endlessly

--- Calculation Methods/Lab2/main.py
+++ Calculation Methods/Lab2/main.py
@@ -3,8 +3,7 @@ import reader
 import logic
 
 
-def main():
-    printer.print_title()
+def start_iteration():
     # Get user input
     m = reader.read_m()
     (A, B) = reader.read_ab()
@@ -12,8 +11,24 @@ def main():
     printer.print_table(table)
     x = reader.read_x()
     n = reader.read_n(m)
-    # Sort x-s
+    # Print results
     printer.print_results(x, n, table)
+
+
+def main():
+    printer.print_title()
+    start_iteration()
+
+    while True:
+        print("Посчитать для нового набора значений? [Y/N]")
+        answer = input()
+
+        if answer.capitalize() == 'Y':
+            start_iteration()
+        elif answer.capitalize() == 'N':
+            break
+        else:
+            continue
 
 
 if __name__ == '__main__':
