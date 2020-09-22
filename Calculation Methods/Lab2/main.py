ec9ebd99a9534903c2744974ce2a4f2b5b09c0a1
Nikita Mamro <nikita.mamro@lanit-tercom.com>
2020-09-22 17:04:05 +0300

    Update main.py

--- Calculation Methods/Lab2/main.py
+++ Calculation Methods/Lab2/main.py
@@ -6,14 +6,24 @@ import settings
 
 def start_iteration():
     # Get user input
-    m = reader.read_m()
     (A, B) = reader.read_ab()
+    m = reader.read_m()
+
     table = logic.create_table(m, A, B, settings.f)
     printer.print_table(table)
-    x = reader.read_x()
-    n = reader.read_n(m)
-    # Print results
-    printer.print_results(x, n, table, settings.f)
+
+    while True:
+        x = reader.read_x()
+        n = reader.read_n(m)
+        # Print results
+        printer.print_results(x, n, table, settings.f)
+        print("Посчитать для новых x и n? [Y/N]")
+        answer = input()
+
+        if answer.capitalize() == 'N':
+            break
+        else:
+            continue
 
 
 def main():
