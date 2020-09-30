7bfa489d7a81ea3d3a8d25ec4edb46656d037089
Nikita Mamro <mamronikita@gmail.com>
2020-09-30 11:17:18 +0300

    Added alphabet pointer task

---
+++ Formal Languages/Test1/concordance.py
@@ -0,0 +1,32 @@
+import io
+import splitter
+
+
+class Concordance:
+
+    def __init__(self):
+        self.__words = {}
+
+    def add_word(self, word, line_number):
+        if word not in self.__words:
+            self.__words[word] = [line_number]
+        elif line_number not in self.__words[word]:
+            self.__words[word].append(line_number)
+
+    def read_text(self, stream):
+        lines = stream.readlines()
+
+        i = 1
+
+        for line in lines:
+            lower_line = line.lower()
+            words = splitter.split(lower_line, [' ', ',', '.', ':', ';'])
+
+            for word in words:
+                self.add_word(word, i)
+
+            i += 1
+
+    def print(self):
+        for word in self.__words:
+            print(word, ' ', ' '.join([str(x) for x in self.__words[word]]))
---
+++ Formal Languages/Test1/main.py
@@ -0,0 +1,14 @@
+import concordance
+import io
+
+
+def main():
+    c = concordance.Concordance()
+    s = io.StringIO('a b;c,d e\na a bd ddx e')
+    # f = open('filename', 'r', encoding='utf-8')
+    c.read_text(s)
+    c.print()
+
+
+if __name__ == '__main__':
+    main()
