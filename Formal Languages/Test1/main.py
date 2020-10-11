6abad8b1c4cae8f7af4e973f2061ab1aa2603e2e
Nikita Mamro <mamronikita@gmail.com>
2020-10-11 15:52:13 +0300

    Rectangles

--- Formal Languages/Test1/main.py
+++ Formal Languages/Test1/main.py
@@ -5,13 +5,12 @@ import manual_tests
 
 
 def main():
-    #c = concordance.Concordance()
-    ## s = io.StringIO('a b;c,d e\na a bd ddx e')
-    # f = codecs.open(
-    #    'input.txt', 'r', 'utf_8_sig')
-    # c.read_text(f)
-    # c.print_result()
-    manual_tests.run_tests()
+    c = concordance.Concordance()
+    # s = io.StringIO('a b;c,d e\na a bd ddx e')
+    f = codecs.open(
+        'input.txt', 'r', 'utf_8_sig')
+    c.read_text(f)
+    c.print_result()
 
 
 if __name__ == '__main__':
