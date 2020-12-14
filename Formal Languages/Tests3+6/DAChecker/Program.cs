99d2d6d6004e0c569c7de0140a4b36d694bc788c
Nikita Mamro <mamronikita@gmail.com>
2020-12-14 16:39:28 +0300

    Added DA minimization task

---
+++ Formal Languages/Tests3+6/DAChecker/Program.cs
@@ -0,0 +1,32 @@
+﻿using DAWorker.Minimizer;
+using DAWorker.Utils;
+using System;
+using System.Linq;
+
+namespace DAWorker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var entryAlphabet = new char[] { 'd', '.', 'e', '±', 'ε' };
+            // Представление матрицы переходов
+            // qi : i (i >= 0, q0 --- начальное состояние)
+            // # : -1
+            // пустая клетка: null
+            var table = new int?[,]
+            {
+                { 1, null, null, null, null, null },
+                { 1, 2, 3, null, null, -1 },
+                { 4, null, null, null, null, null },
+                { 5, null, null, null, 6, null },
+                { 4, null, null, 3, null, -1 },
+                { 5, null, null, null, null, -1 },
+                { 5, null, null, null, null, null },
+            };
+
+            var minimizer = new DAMinimizer();
+            minimizer.MinimizeDA(table, entryAlphabet);
+        }
+    }
+}
